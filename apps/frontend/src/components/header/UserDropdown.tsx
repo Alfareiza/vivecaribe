235132114a46eb19515ff8da0ea9a9a5ff9cbbad
Alfonso <alfareiza@gmail.com>
2026-08-11 10:27:21 -0300

    Ignored some elements that were part of the template

--- apps/frontend/src/components/header/UserDropdown.tsx
+++ apps/frontend/src/components/header/UserDropdown.tsx
@@ -33,7 +33,7 @@ export default function UserDropdown() {
           <Image
             width={44}
             height={44}
-            src="/images/user/owner.jpg"
+            src="/images/user/owner.jpeg"
             alt="User"
           />
         </span>
